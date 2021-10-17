fc0d6e8801bdf6f3a228dc11dad74756693b7d33
vimalshah25git config --global user.email vimalshah2510@gmail.com <vimalshah2510@gmail.comgit config --global lgit config --global-l!git commit -m first>
2021-10-17 19:29:51 +0100

    i like git

---
+++ src/test3.java
@@ -0,0 +1,11 @@
+public class test3 {
+    public static void main(String[] args) {
+
+        System.out.println("i like git ");
+        System.out.println("i like git");
+        System.out.println("i like git ");
+
+
+
+    }
+}
